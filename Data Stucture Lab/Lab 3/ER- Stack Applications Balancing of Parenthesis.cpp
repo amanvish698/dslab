1fde43185c72d5eb2977b143c5ea1f2a5fa074ef
amanvish698 <157875964+amanvish698@users.noreply.github.com>
2024-01-27 23:49:21 +0530

    Update ER- Stack Applications Balancing of Parenthesis.cpp

--- Data Stucture Lab/Lab 3/ER- Stack Applications Balancing of Parenthesis.cpp
+++ Data Stucture Lab/Lab 3/ER- Stack Applications Balancing of Parenthesis.cpp
@@ -105,3 +105,109 @@ cout<<"Expression is Not Balanced";
 getch(); 
  return 0; 
 } 
+
+
+
+// Not Balanced
+
+
+// #include<iostream> 
+// #include<conio.h> 
+// #include <string.h> 
+// using namespace std; 
+
+// class stacks {
+// private: 
+//     char a[10];
+//     int top1;
+
+// public: 
+//     stacks() {
+//         top1 = -1; 
+//     }
+
+//     void push(char);
+//     int pop();
+//     char top();
+//     bool areParanthesisBalanced(char[]);
+// };
+
+// void stacks::push(char n) {
+//     if (top1 == 9) {
+//         cout << "Stack Overflow" << endl; 
+//     } else {
+//         ++top1; 
+//         a[top1] = n; 
+//         cout << n << " is pushed onto the stack " << endl; 
+//     }
+// }
+
+// int stacks::pop() {
+//     if (top1 == -1) {
+//         cout << "Stack Underflow" << endl; 
+//     } else {
+//         return a[top1--]; 
+//     }
+// }
+
+// char stacks::top() {
+//     if (top1 == -1) {
+//         cout << "Stack is Empty" << endl;
+//         return '\0'; // Return a default character in case of an empty stack
+//     } else {
+//         return a[top1]; 
+//     }
+// }
+
+// bool stacks::areParanthesisBalanced(char expr[]) {
+//     for (int i = 0; i < strlen(expr); i++) {
+//         if (expr[i] == '(' || expr[i] == '[' || expr[i] == '{') {
+//             push(expr[i]); 
+//         } else {
+//             if (top1 == -1) {
+//                 return false; 
+//             }
+
+//             char popped = pop(); // Store the popped element
+
+//             switch (expr[i]) {
+//                 case ')':
+//                     if (popped != '(') {
+//                         cout << "Not Balanced" << endl; 
+//                         return false; 
+//                     }
+//                     break; 
+//                 case '}':
+//                     if (popped != '{') {
+//                         cout << "Not Balanced" << endl; 
+//                         return false; 
+//                     }
+//                     break; 
+//                 case ']':
+//                     if (popped != '[') {
+//                         cout << "Not Balanced" << endl; 
+//                         return false; 
+//                     }
+//                     break; 
+//             }
+//         }
+//     }
+
+//     // Check if the stack is empty after processing the expression
+//     return top1 == -1; 
+// }
+
+// int main() {
+//     stacks s; 
+//     char expr[] = "{([)]}"; 
+
+//     if (s.areParanthesisBalanced(expr)) {
+//         cout << "Expression is Balanced" << endl; 
+//     } else {
+//         cout << "Expression is Not Balanced" << endl; 
+//     }
+
+//     getch(); 
+//     return 0; 
+// }
+
